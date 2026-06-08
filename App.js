2ace09bdc91f1fb8884e3844e210dfd3119aa2c1
rajeshmaragani36 <rajeshmaragani36@gmail.com>
2026-06-08 20:18:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./components/Home";
+import ComponentsDemo from "./components/ComponentsDemo";
+import PropsDemo from "./components/PropsDemo";
+import StateDemo from "./components/StateDemo";
+import About from "./components/About";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/components">Components</Link>
+          <Link to="/props">Props</Link>
+          <Link to="/state">State</Link>
+          <Link to="/about">About</Link>
+        </nav>
+
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/components" element={<ComponentsDemo />} />
+            <Route path="/props" element={<PropsDemo />} />
+            <Route path="/state" element={<StateDemo />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
