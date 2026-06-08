0225c0a3daca74524897abab8f341015fc1e7031
rajeshmaragani36 <rajeshmaragani36@gmail.com>
2026-06-08 21:28:19 +0530

    Use HashRouter

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import Home from "./components/Home";
 import ComponentsDemo from "./components/ComponentsDemo";
